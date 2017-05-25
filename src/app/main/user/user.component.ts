28ca92c2dd9b5254a781087fee0775ecd2e532ae
Hau Le <lethehau90.vn@gmail.com>
2017-05-25 23:53:00 +0700

    update

--- src/app/main/user/user.component.ts
+++ src/app/main/user/user.component.ts
@@ -5,6 +5,7 @@ import { NotificationService } from '../../core/services/notification.service';
 import { MessageContstants } from '../../core/common/message.constants';
 
 import { IMultiSelectOption } from 'angular-2-dropdown-multiselect';
+declare var moment: any;
 
 @Component({
     selector: 'app-user',
@@ -25,20 +26,26 @@ export class UserComponent implements OnInit {
     public users: any[];
     public entity: any;
 
+
     public allRoles: IMultiSelectOption[] = [];
     public roles = [];
+    
 
-    constructor(private dataService: DataService) { }
+    constructor(private dataService: DataService, private notificationService: NotificationService) { }
 
     ngOnInit() {
         this.loadRoles();
-        this.loadUser();
+        this.loadData();
     }
 
-    loadUser(): void {
+    loadData(): void {
         this.dataService.get('/api/appUser/getlistpaging?page=' + this.pageIndex + '&pageSize=' + this.pageSize + '&filter=' + this.filter)
             .subscribe((response: any) => {
-                this.users = response.Items
+                console.log(response)
+                this.users = response.Items;
+                this.pageIndex = response.PageIndex;
+                this.pageSize = response.PageSize;
+                this.totalRow = response.TotalRows;
             })
     }
 
@@ -48,6 +55,18 @@ export class UserComponent implements OnInit {
             for (let role of response) {
                 this.allRoles.push({ id: role.Name, name: role.Description });
             }
+        }, error => this.dataService.handleError(error));
+    }
+
+    loadIdUser(id: any): void {
+        this.dataService.get('/api/appUser/detail/' + id).subscribe((response: any[]) => {
+            this.modalAddEdit.show();
+            this.entity = response;
+            console.log(this.entity)
+            for (let role of this.entity.Roles) {
+                this.myRoles.push(role);
+            }
+            this.entity.BirthDay = moment(new Date(this.entity.BirthDay)).format('DD/MM/YYYY');
         }, error => this.dataService.handleError(error));
     }
 
@@ -62,8 +81,40 @@ export class UserComponent implements OnInit {
         this.modalAddEdit.show();
     }
 
+    showEditModel(id: any): void {
+        this.loadIdUser(id);
+    }
+
     public selectGender(event) {
         this.entity.Gender = event.target.value
     }
 
+    saveChange(valid: boolean) {
+        if (valid) {
+            if (this.entity.id === undefined) {
+                this.dataService.post('/api/appUser/add', JSON.stringify(this.entity)).subscribe((response: any) => {
+                    this.notificationService.printSuccessMessage(MessageContstants.CREATED_OK_MSG);
+                    this.loadData();
+                    this.modalAddEdit.hide();
+                }, error => this.dataService.handleError(error))
+            }
+            else {
+                this.dataService.put('/api/appUser/update', JSON.stringify(this.entity)).subscribe((response: any) => {
+                    this.notificationService.printSuccessMessage(MessageContstants.UPDATED_OK_MSG);
+                    this.loadData();
+                    this.modalAddEdit.hide();
+                }, error => this.dataService.handleError(error))
+            }
+        }
+    }
+
+    deleteItem(id: any) {
+        this.notificationService.printConfirmationDialog(MessageContstants.CONFIRM_DELETE_MSG, () => this.deleteItemUserId(id));
+    }
+    deleteItemUserId(id: any) {
+        this.dataService.delete('/api/appUser/delete', 'id', id).subscribe((response: any) => {
+            this.loadData();
+            this.notificationService.printSuccessMessage(MessageContstants.DELETED_OK_MSG)
+        })
+    }
 }
