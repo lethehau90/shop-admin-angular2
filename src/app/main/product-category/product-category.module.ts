f297fc1188aa881c938193b265df9891d1e873c4
Hau Le <lethehau90.vn@gmail.com>
2017-06-05 20:42:34 +0700

    add category product

--- src/app/main/product-category/product-category.module.ts
+++ src/app/main/product-category/product-category.module.ts
@@ -1,11 +1,25 @@
-import { NgModule } from '@angular/core';
+﻿import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { ProductCategoryComponent } from './product-category.component';
+import { ProductCategoryRouter } from './product-category.routes';
+
+import { Routes, RouterModule } from '@angular/router';
+import { TreeModule } from 'angular-tree-component';
+import { FormsModule } from '@angular/forms';
+import { ModalModule } from 'ngx-bootstrap';
+
+//import { DataService } from './../../core/services/data.service';
+//import { UtilityService } from './../../core/services/utility.service';
 
 @NgModule({
-  imports: [
-    CommonModule
-  ],
-  declarations: [ProductCategoryComponent]
+    imports: [
+        CommonModule,
+        ProductCategoryRouter,
+        TreeModule,
+        FormsModule,
+        ModalModule
+    ],
+    declarations: [ProductCategoryComponent],
+    //providers: [DataService, UtilityService]
 })
 export class ProductCategoryModule { }
