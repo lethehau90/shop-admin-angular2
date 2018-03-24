a7c9f143cae5db909902e30fca918bbaf8f8994e
LEHAU\ADMIN <lethehau90.vn@gmail.com>
2018-03-25 00:27:55 +0700

    update check jquery affer load

--- src/app/main/report/report.routes.ts
+++ src/app/main/report/report.routes.ts
@@ -1,8 +1,10 @@
 import { Routes, RouterModule } from '@angular/router';
 import { RevenueComponent } from './revenue/revenue.component';
+import { VisitorComponent } from './visitor/visitor.component';
 const routes : Routes = [
     { path : '', redirectTo : 'index', pathMatch : 'full' },
     { path: 'index', component : RevenueComponent },
-    { path: 'revenue', component: RevenueComponent }
+    { path: 'revenue', component: RevenueComponent },
+    { path: 'visitor', component: VisitorComponent }
 ];
 export const ReportRouter = RouterModule.forChild(routes);
